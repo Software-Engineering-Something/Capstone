d293add59e9e8a7866933309e64f94a2152d8680
tempshadow <nigel.mansell-1@ou.edu>
2019-10-20 12:09:27 -0500

    Added classes, some of their functionality, as we as comments. Main has a test case.

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from Quarter import Quarter
 
 def main():
     """Testing a case. May clean this up by adding methods in associated classes"""
-    temp = Company("msci", "something")
-    temp.years.append(Year(2014))
-    print(temp.years[0].year)
-    temp.years[0].quarters.append(Quarter("Q1"))
-    print(temp.years[0].quarters[0].quarter)
-    temp.years[0].quarters[0].assets.unit = "US Dollars"
-    print(temp.years[0].quarters[0].assets.unit)
-    temp.years[0].quarters[0].assets.amount = 24.50
-    print(temp.years[0].quarters[0].assets.amount)
+    msci = Company("msci", "something")
+    msci.years.append(Year(2014))
+    print(msci.years[0].year)
+    msci.years[0].quarters.append(Quarter("Q1"))
+    print(msci.years[0].quarters[0].quarter)
+    msci.years[0].quarters[0].assets.unit = "US Dollars"
+    print(msci.years[0].quarters[0].assets.unit)
+    msci.years[0].quarters[0].assets.amount = 24.50
+    print(msci.years[0].quarters[0].assets.amount)
 
 
 if __name__ == '__main__':
