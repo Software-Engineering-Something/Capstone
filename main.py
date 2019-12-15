44fca41b61ad9b80cd511067a8950687e2b61157
tempshadow <nigel.mansell-1@ou.edu>
2019-12-15 05:14:08 -0600

    Added read function to main. Currently reads file, looks for company name as well as that companies ticker then creates object for said company and stores it into an array

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import csv
 from Company import Company
 from Year import Year
 from Quarter import Quarter
+import csv
 
 
 def main():
     """Testing a case. May clean this up by adding methods in associated classes"""
-    msci = Company("msci", "something")
+    """msci = Company("msci", "something")
     msci.years.append(Year(2014))
     print(msci.years[0].year)
     msci.years[0].quarters.append(Quarter("Q1"))
@@ -14,7 +15,22 @@ def main():
     msci.years[0].quarters[0].assets.unit = "US Dollars"
     print(msci.years[0].quarters[0].assets.unit)
     msci.years[0].quarters[0].assets.amount = 24.50
-    print(msci.years[0].quarters[0].assets.amount)
+    print(msci.years[0].quarters[0].assets.amount)"""
+    test = []
+    read("fundamentals_dataset.csv", test)
+    for i in range(len(test)):
+        print(test[i].name + ", " + test[i].ticker)
+
+
+def read(file, test):
+    companynames = []
+    with open(file) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            if row[1] not in companynames and row[1] != "company":
+                companynames.append(row[1])
+                company = Company(row[1], row[2])
+                test.append(company)
 
 
 if __name__ == '__main__':
