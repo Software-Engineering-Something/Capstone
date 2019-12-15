5a5e2d9fd38c4385ba8035e77bb226a0576e759b
tempshadow <nigel.mansell-1@ou.edu>
2019-12-15 04:19:17 -0600

    Changed how a global array was being created. Moved to self.

--- Company.py
+++ Company.py
@@ -1,9 +1,8 @@
 class Company:
-    """Class level list holds year objects"""
-    years = []
 
     """Constructor allows for built in getter and setter for parameters"""
     def __init__(self, name, ticker):
         self.name = name
         self.ticker = ticker
+        self.years = []
 
--- Year.py
+++ Year.py
@@ -1,7 +1,6 @@
 class Year:
-    """Class level list holds quarter objects"""
-    quarters = []
 
     """Constructor allows for built in getter and setter of year"""
     def __init__(self, year):
         self.year = year
+        self.quarters = []
