1b12b3de18060f70798b54134c6715d4321a5654
tempshadow <nigel.mansell-1@ou.edu>
2020-01-21 10:27:59 -0600

    Added new file, dropped some columns

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import dask.dataframe as dd
-import pandas as pd
+import pandas
+import math
+import random
 
 
 def main():
@@ -7,13 +9,25 @@ def main():
     csv file and parses into data. From there we can use query like calls to search for specific data at O(N) time,
     I'm still new to dataframes, so doing these two search queries may somehow be done in line"""
 
-    df = pd.read_csv("fundamentals_dataset.csv")
-    ddf = dd.from_pandas(df, npartitions=2)
-    new = df[df.company == '1347 Property Insurance Holdings, Inc.'][df.period == '2014 Q1']
-    new['amount'] = new['amount'].str.replace(',', '')
-    print(new[new['amount'].astype(int) > 20000000])
-    temp = new[new['amount'].astype(int) > 20000000]
-    print("getting only the second rows amount: " + temp['amount'].iloc[1])
+    df = pandas.read_csv("aadr.us.csv")
+    df = df.drop(None,0,None,'Date',None, False, 'raise')
+    df = df.drop(None,0,None,'Open',None, False, 'raise')
+    df = df.drop(None, 0, None, 'High', None, False, 'raise')
+    df = df.drop(None, 0, None, 'Low', None, False, 'raise')
+    df = df.drop(None, 0, None, 'OpenInt', None, False, 'raise')
+    print(df)
+    """from numpy.random import permutation
+    random_indices = permutation(new.index)
+    test_cutoff = math.floor(len(new)/3)
+    test = new.loc[random_indices[1:test_cutoff]]
+    train = new.loc[random_indices[test_cutoff:]]
+    x_columns = ['amount']
+    y_column = ['amount']
+
+    from sklearn.neighbors import KNeighborsRegressor
+    knn = KNeighborsRegressor(n_neighbors=5)
+    knn.fit(train[x_columns], train[y_column])
+    predictions = knn.predict(test[x_columns])"""
 
 
 if __name__ == '__main__':
